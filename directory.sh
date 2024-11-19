8a84816d581471fcd5a64b800d4f9e5719c6e96f
Saeed Roustaei <saeedrs71@yahoo.com>
2024-11-19 10:56:59 +0330

    Finding a directory was solved

--- directory.sh
+++ directory.sh
@@ -5,7 +5,7 @@ case "$name" in
 	"Mahdi")
 		read -s -p "Password: " pass
 		while [ ${#pass} -lt 8 ];do
-			echo -e "Password character must be more than 8!\nPassword: "
+			echo -e "\nPassword character must be more than 8!\nPassword: "
 	                read -s  pass
 		  done     	
 		echo -e "\nYou are authorized"
@@ -22,13 +22,14 @@ case "$name" in
 	  echo "You are not Authorized!"
 	  exit
 esac  	  
-sleep 5
+sleep 1
 clear 
 read -p " please enter a directory: " directory
-if [ -d "/$directory" ];then
-	echo "Directory is availble"
-        find /$directory -name $directory -type d 2>/dev/null
+x=$(find / -type d -name $directory 2>/dev/null)
+if [[ $x == *"$directory"*  ]];then
+	echo "Directory is availble"   
 else
+	echo "Directory is not found"
 	echo -e  " Directory not found \nDo you want to create $directory ?(y/n)"
 	read answer
 	if [ "$answer" == "y" ];then
